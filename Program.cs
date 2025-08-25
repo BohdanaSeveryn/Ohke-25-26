55b41e02a085e99fe884722940d4010a7cca6df4
P17966 <p17966@edu.sasky.fi>
2025-08-25 13:22:33 +0300

    UI Element added to Program.cs
    Added UI that uses while loop to show the menu options (which is probably not needed for this exercise but whatever) Switch to use as option to select instead of loads of if and else if statements. It has Method added for testing purposes which you can delete when when we have rest of the program done.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Using loop as menu
+        while (true)
+        {
+            //Writeline menu options
+            Console.WriteLine("Alkuaine-testi");
+            Console.WriteLine("Haluatko pelata? (p)");
+            Console.WriteLine("Haluatko tarkastella tuloksia? (t)");
+            Console.WriteLine("Haluatko lopettaa? (l)");
+            //Write on same line as input
+            Console.Write("Valintasi: ");
+            //Read input and convert to lowercase
+            string? valinta = Console.ReadLine()!.ToLower();
+            //Switch case for menu options
+            switch (valinta)
+            {
+                case "p":
+                    TemporaryMethod();
+                    break;
+                case "t":
+                    TemporaryMethod();
+                    break;
+                case "l":
+                    Console.WriteLine("Lopetetaan ohjelma.");
+                    return;
+                default:
+                    Console.WriteLine("Virheellinen valinta. Yritä uudelleen.");
+                    break;
+            }
+        }
     }
+    //Temporary method for testing UI
+    static void TemporaryMethod()
+    {
+        Console.WriteLine("This is a Method only for testing purposes.");
+    }
+
 }
